0a77e3951b1398219efcf6861bd7163bded5cd43
Kazutake Yamamoto <french0719@gmail.com>
2023-11-21 20:35:20 +0900

    Update 1.two-sum.kt

--- easy/1.two-sum.kt
+++ easy/1.two-sum.kt
@@ -1,13 +1,15 @@
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        val ans = mutableListOf<Int>()
-        nums.forEach {
-            // addしても追加されていないので続きここから
-            if(target === 7) {
-                ans.add(target)
-                ans.add(5)
+        val list = mutableListOf<Int>()
+        outloop@ for(x in 0..nums.size-1) {
+            for(y in x+1..nums.size-1) {
+                if(target == nums[x] + nums[y]) {
+                    list.add(x)
+                    list.add(y)
+                    break@outloop
+                }
             }
         }
-        return ans.toIntArray()
+        return list.toIntArray()
     }
 }
