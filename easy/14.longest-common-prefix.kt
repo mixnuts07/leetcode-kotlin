3b544dfb413d81b2022cc98b05f3f21f67b1a512
Kazutake Yamamoto <french0719@gmail.com>
2023-11-23 21:06:29 +0900

    Update 14.longest-common-prefix.kt

--- easy/14.longest-common-prefix.kt
+++ easy/14.longest-common-prefix.kt
@@ -1,11 +1,29 @@
 class Solution {
     fun longestCommonPrefix(strs: Array<String>): String {
-        val output = ""
-        outloop@ for(i in 1..strs.size-1){
-            if (strs[0].first() != strs[i].first()){
-                break@outloop
+    var output = ""
+    when (strs[0].length) {
+        0 -> output = ""
+        else {
+            outloop@ for(i in 1 until strs.size){ 
+                for(j in 0..strs[i].length){
+                    if (strs[0].first() != strs[i].first()) {
+                        output = ""
+                        break@outloop
+                    } 
+                    else if(strs[0].substring(0,j) == strs[i].substring(0,j)) {
+                        output = strs[0].substring(0,j)
+                    }    
+                }
+
+                }
             }
         }
         return output
+    }
+    fun longestCommonPrefix(strs: Array<String>) {
+        for(i in 0 until strs.size) {
+            // strs[0].commonPrefixWith(strs[])
+                println(i)
+        }
     }
 }
